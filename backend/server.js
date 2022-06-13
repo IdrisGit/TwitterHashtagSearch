893af9f2bb2e5633d4117fe4ddcd603237b8166f
IdrisGit <idris.gadi01@gmail.com>
2022-06-14 00:10:13 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key:         'psn8fBs3uxAdDwTT2Bk1SgFgm',
+    consumer_secret:      'gRCEBxaZPgBhrQcdaj1y5oxWYft4Lp7E94vUJZqp4AGFm02pSc',
+    access_token:         '1536366926213136387-s7pevcNPhCFRbgukqi5NqumJ09yQxb',
+    access_token_secret:  'Pu9dGAkupErLE9wVhcu6ET2g24uQTiIx6LEhtdKyptuMn',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  })
+
+const app = express();
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+T.get('search/tweets', { q: '#football', count: 2 }, function(err, data, response) {
+    console.log(data.statuses[0].entities)
+})
+
+
+app.listen(3000,()=>{
+    console.log('app is working on port 3000')
+})
